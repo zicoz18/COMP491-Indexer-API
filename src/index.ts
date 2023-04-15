5395a0a11564a63b8629821a7ef0b2e869d6c234
zicoz18 <zicoz18@ku.edu.tr>
2023-04-16 01:10:35 +0300

    Allow search wtih transactionHash and blockNumber

--- src/index.ts
+++ src/index.ts
@@ -14,19 +14,58 @@ const main = async () => {
   // Parse JSON bodies for this app
   app.use(bodyParser.json())
 
-  // Simple "Hello World" endpoint
   app.get('/blocks', async (req, res) => {
-    const blocks = await collections.block.find({}).sort({ number: -1 }).limit(2).toArray()
-    res.send(blocks)
+    // If block number is given, return the specific block
+    if (req.query.number) {
+      const block = await collections.block.findOne({
+        number: parseInt(req.query.number as unknown as string),
+      })
+      res.send(block)
+      return
+    } else if (req.query.latestCount) {
+      // If latestCount is indicated return latest `latestCount` amount of blocks
+      const blocks = await collections.block
+        .find({})
+        .sort({ number: -1 })
+        .limit(parseInt(req.query.latestCount as unknown as string))
+        .toArray()
+      res.send(blocks)
+      return
+    } else {
+      // If latestCount is not indicated return latest `10` amount of transactions
+      const blocks = await collections.block.find({}).sort({ number: -1 }).limit(10).toArray()
+      res.send(blocks)
+      return
+    }
   })
 
   app.get('/transactions', async (req, res) => {
-    const transactions = await collections.transaction
-      .find({})
-      .sort({ blockNumber: -1 })
-      .limit(2)
-      .toArray()
-    res.send(transactions)
+    // If transaction hash is given, return the specific transaction
+    if (req.query.hash) {
+      console.log(req.query.hash)
+      const transaction = await collections.transaction.findOne({ hash: req.query.hash })
+      console.log(transaction)
+      res.send(transaction)
+      return
+    } else if (req.query.latestCount) {
+      // If latestCount is indicated return latest `latestCount` amount of transactions
+      const transactions = await collections.transaction
+        .find({})
+        .sort({ blockNumber: -1 })
+        .limit(parseInt(req.query.latestCount as unknown as string))
+        .toArray()
+      res.send(transactions)
+      return
+    } else {
+      // If latestCount is not indicated return latest `10` amount of transactions
+      const transactions = await collections.transaction
+        .find({})
+        .sort({ blockNumber: -1 })
+        .limit(10)
+        .toArray()
+      res.send(transactions)
+      return
+    }
   })
 
   // Start the server
